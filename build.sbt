45cae4c57ecf622a06f847b4a84274fe81d6cc9b
felipeOso <filip900215@gmail.com>
2021-09-17 21:06:05 -0500

    commit Welcome

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "FunctionalProgramming"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/FunctionalProgramming.scala
@@ -0,0 +1,7 @@
+object FunctionalProgramming  {
+    def main(args: Array[String]): Unit = {
+    println("**** Functional Programming ****")
+      println("Values and Declarations")
+  }
+
+}
